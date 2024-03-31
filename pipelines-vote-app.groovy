0aca913b416da62926b4fce5e35dbad0cf3216fc
Zia Ansari <ansariziatec@gmail.com>
2024-03-31 15:43:48 +0530

    Update pipelines-vote-app.groovy

--- pipelines-vote-app.groovy
+++ pipelines-vote-app.groovy
@@ -8,25 +8,6 @@ pipeline {
                 git branch: "main" , url: "https://github.com/ansarizia/pipelines-vote-ui.git"
             }
         }
-        stage('Docker Build') {
-            steps{
-            sh "docker build -t ansarizia/vote-ui:latest . "
-            }
-        }
-        stage('Docker Push'){
-            steps{
-                
-                withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId:'dockerhub', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD']]) {
-                sh "docker login -u $USERNAME -p $PASSWORD"
-                sh "docker push ansarizia/vote-ui:latest"
-                }
-            }
-        }
-        stage('Deploy'){
-            steps{
-                sh "kubectl apply -f k8s/deployment.yaml --kubeconfig /home/ziansari/.kube/config"
-                sh "kubectl apply -f k8s/service.yaml --kubeconfig /home/ziansari/.kube/config"
-            }
-        }
+        
     }
 }
