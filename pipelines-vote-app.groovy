a38fd9229ccaed6291898513f5726f832dfe5d8f
Zia Ansari <ansariziatec@gmail.com>
2024-04-10 14:48:50 +0530

    Update pipelines-vote-app.groovy
    Adding params in Jenkins file

--- pipelines-vote-app.groovy
+++ pipelines-vote-app.groovy
@@ -1,10 +1,19 @@
 pipeline {
     agent any
-
+    properties([
+        parameters([
+            choice(name: 'BRANCH_NAME',choices: ['master','feature'], description:'Name of branch'),
+            string(
+                defaultValue: 'feature',
+                name: 'branchName'
+            )
+        ])
+    ])
     stages {
         stage('Clone Repo') {
             steps {
                 echo "Checkout in progress "
+                echo params.BRANCH_NAME
                 git branch: "main" , url: "https://github.com/ansarizia/pipelines-vote-ui.git"
             }
         }
